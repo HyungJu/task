b3a2f1af0f08a3524a166ca76ebd465fa4b86618
HyungJu <sungkisa@naver.com>
2023-01-20 18:49:13 +0900

    feat: implement create-currency mutation

--- src/currency/application/services/create-currency.ts
+++ src/currency/application/services/create-currency.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Currency } from '../../domain/models/currency.model';
 import { CurrencyRepositoryImpl } from '../../infrastructure/repository/currency.repository';
+import { DuplicateCurrencyCodeException } from '../../domain/exceptions/DuplicateCurrencyCodeException';
 
 export class CreateCurrencyInputDto {
   code!: string;
@@ -12,6 +13,9 @@ export class CreateCurrency {
   constructor(private currencyRepository: CurrencyRepositoryImpl) {}
 
   public async execute(dto: CreateCurrencyInputDto) {
+    const currencyExists = await this.currencyRepository.findByCode(dto.code);
+    if (currencyExists) throw new DuplicateCurrencyCodeException();
+
     const currency = new Currency(dto.code, dto.name);
     await this.currencyRepository.insert(currency);
 
--- src/currency/presentation/resolvers/currency.resolvers.ts
+++ src/currency/presentation/resolvers/currency.resolvers.ts
@@ -1,22 +1,31 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { FindCurrency } from '../../application/services/find-currency';
 import { GetAllCurrencies } from '../../application/services/get-all-currencies';
 import { CurrencySchema } from '../schemas/currency.schema';
+import { CreateCurrencyInput } from '../schemas/create-currency.schema';
+import { CreateCurrency } from '../../application/services/create-currency';
 
 @Resolver((of) => CurrencySchema)
 export class CurrencyResolver {
   constructor(
-    private findCurrency: FindCurrency,
-    private getAllCurrencies: GetAllCurrencies,
+    private findCurrencyService: FindCurrency,
+    private getAllCurrenciesService: GetAllCurrencies,
+    private createCurrencyService: CreateCurrency,
   ) {}
 
   @Query((returns) => CurrencySchema, { nullable: true })
   async currency(@Args('code', { type: () => String }) code: string) {
-    return this.findCurrency.execute(code);
+    return this.findCurrencyService.execute(code);
   }
 
   @Query(() => [CurrencySchema])
   async currencies() {
-    return this.getAllCurrencies.execute();
+    return this.getAllCurrenciesService.execute();
+  }
+
+  @Mutation((returns) => CurrencySchema)
+  createCurrency(@Args('createCurrencyInput') input: CreateCurrencyInput) {
+    console.log(input);
+    return this.createCurrencyService.execute(input);
   }
 }
