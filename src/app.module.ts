b321db8bb69f212a087152663ea1811969f825d4
HyungJu <sungkisa@naver.com>
2023-01-20 18:18:12 +0900

    chore: configure graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CurrencyModule } from './currency/currency.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/buzznbyd'),
     CurrencyModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: process.cwd() + '/src/schema.gql',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
