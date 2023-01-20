ff346eebbd214381e772375bc2aa450429d88b39
HyungJu <sungkisa@naver.com>
2023-01-20 22:55:32 +0900

    feat: query exchange by currency id

--- src/exchange/domain/exchange.repository.ts
+++ src/exchange/domain/exchange.repository.ts
@@ -1,7 +1,10 @@
 import { ExchangeRate } from './models/exchange.model';
+import { Currency } from '@currency/domain/models/currency.model';
 
 export interface ExchangeRepository {
   findAll(): Promise<ExchangeRate[]>;
 
   create(exchangeRate: ExchangeRate): Promise<ExchangeRate>;
+
+  get(from: Currency, to: Currency): Promise<ExchangeRate>;
 }
--- src/exchange/infrastructure/repository/exchange.repository.ts
+++ src/exchange/infrastructure/repository/exchange.repository.ts
@@ -3,7 +3,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { ExchangeRepository } from '../../domain/exchange.repository';
 import { ExchangeModel } from '../schemas/exchange.schema';
 import { ExchangeRate } from '../../domain/models/exchange.model';
-import { CurrencyModel } from '../../../currency/infrastructure/schemas/currency.schema';
 
 @Injectable()
 export class ExchangeRepositoryImpl implements ExchangeRepository {
@@ -17,14 +16,11 @@ export class ExchangeRepositoryImpl implements ExchangeRepository {
     return this.exchangeModel.create(exchangeRate);
   }
 
-  public async get(
-    from: CurrencyModel,
-    to: CurrencyModel,
-  ): Promise<ExchangeRate> {
+  public async get(from: Currency, to: Currency): Promise<ExchangeRate> {
     return this.exchangeModel
       .findOne({
-        from: from,
-        to: to,
+        from: from._id,
+        to: to._id,
       })
       .populate(['from', 'to']);
   }
