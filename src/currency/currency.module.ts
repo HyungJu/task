9c35de1c3b1abe55d3e9f4f777529431441de019
HyungJu <sungkisa@naver.com>
2023-01-20 18:41:03 +0900

    refactor: move resolvers to presentation layer

--- src/currency/currency.module.ts
+++ src/currency/currency.module.ts
@@ -2,7 +2,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CreateCurrency } from './application/services/create-currency';
 import { Currency } from './domain/models/currency.model';
 import { CurrencySchema } from './infrastructure/schemas/currency.schema';
-import { CurrencyResolver } from './resolvers/currency.resolvers';
+import { CurrencyResolver } from './presentation/resolvers/currency.resolvers';
 import { FindCurrency } from './application/services/find-currency';
 import { GetAllCurrencies } from './application/services/get-all-currencies';
 import { CurrencyRepositoryImpl } from './infrastructure/repository/currency.repository';
--- src/currency/presentation/resolvers/currency.resolvers.ts
+++ src/currency/presentation/resolvers/currency.resolvers.ts
@@ -1,7 +1,7 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { FindCurrency } from '../application/services/find-currency';
-import { GetAllCurrencies } from '../application/services/get-all-currencies';
-import { CurrencySchema } from '../presentation/schemas/currency.schema';
+import { FindCurrency } from '../../application/services/find-currency';
+import { GetAllCurrencies } from '../../application/services/get-all-currencies';
+import { CurrencySchema } from '../schemas/currency.schema';
 
 @Resolver((of) => CurrencySchema)
 export class CurrencyResolver {
