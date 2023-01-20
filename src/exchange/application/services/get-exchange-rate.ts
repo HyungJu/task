01e535c0d0b71c8f4cd25c60f7f8c70892212606
HyungJu <sungkisa@naver.com>
2023-01-20 23:04:27 +0900

    refactor: remove any casts

--- src/exchange/application/services/get-exchange-rate.ts
+++ src/exchange/application/services/get-exchange-rate.ts
@@ -1,8 +1,9 @@
 import { ApplicationService } from '../../../core/application-service';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ExchangeRate } from '../../domain/models/exchange.model';
 import { ExchangeRepositoryImpl } from '../../infrastructure/repository/exchange.repository';
 import { FindCurrency } from '../../../currency/application/services/find-currency';
+import { CurrencyNotFouncException } from '@exchange/domain/exceptions/CurrencyNotFouncException';
 
 export type GetExchangeRateInput = {
   from: string;
@@ -19,10 +20,10 @@ export class GetExchangeRate
   ) {}
 
   public async execute(input: GetExchangeRateInput): Promise<ExchangeRate> {
-    const from = (await this.findCurrency.execute(input.from)) as any;
-    const to = (await this.findCurrency.execute(input.to)) as any;
+    const from = await this.findCurrency.execute(input.from);
+    const to = await this.findCurrency.execute(input.to);
+    if (!from || !to) throw new CurrencyNotFouncException();
 
-    if (!from || !to) throw new NotFoundException('NO CURRENCY');
     return this.exchangeRepository.get(from, to);
   }
 }
